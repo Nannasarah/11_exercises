57969493ca9c7121ab1ad82d43b4e57291932f69
Nannasarah <nannasp01@gmail.com>
2025-09-05 10:48:48 +0200

    toUpperCaseogSlice

---
+++ 3caPitalization/js.js
@@ -0,0 +1,13 @@
+const str = "nanna";
+console.log(str);
+
+//lav str til capslock
+console.log(str.toUpperCase());
+
+//gør at 3. bogstav er stor:
+const tredjeBogstavStort =
+  str.slice(0, 2) + //na
+  str.slice(2, 3).toUpperCase() + //N
+  str.slice(3); //na
+
+console.log(tredjeBogstavStort);
