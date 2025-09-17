5c4c6b47f1da65f98d1e00f4240422f6863d4f05
Nannasarah <nannasp01@gmail.com>
2025-09-17 15:33:42 +0200

    todoapp

--- ThemeSwitcher/script.js
+++ ThemeSwitcher/script.js
@@ -1,3 +1,7 @@
+// const storageTheme = localStorage.getItem("storageTheme");
+// console.log("storageTheme", storageTheme);
+
+//skifter tema
 document.addEventListener("DOMContentLoaded", () => {
   const select = document.querySelector(".theme");
   const body = document.body;
@@ -6,3 +10,19 @@ document.addEventListener("DOMContentLoaded", () => {
     body.setAttribute("data-theme", event.target.value);
   });
 });
+
+//klaus
+// document.querySelector("#theme_select").addEventListener("change", thChange);
+// if (storageTheme === null) {
+// } else {
+//   document.querySelector("body").dataset.theme = storageTheme;
+// }
+
+// function thChange(evt) {
+//   const chosenTheme = evt.target.value;
+//   console.log("chosenTheme", chosenTheme);
+//   localStorage.setItem("storageTheme", chosenTheme);
+//   document.querySelector("body").dataset.theme = chosenTheme;
+// }
+
+//gemmer i local storage
